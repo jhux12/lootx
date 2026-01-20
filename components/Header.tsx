ea413cbec38174656dc818fe37daf697d026a245
jhux12 <79742669+jhux12@users.noreply.github.com>
2026-01-20 18:16:00 -0500

    Increase header logo size

--- components/Header.tsx
+++ components/Header.tsx
@@ -137,7 +137,7 @@ export const Header: React.FC = () => {
             <img 
               src={pullzLogo} 
               alt="PULLZ Logo" 
-              className="h-10 md:h-12"
+              className="h-12 md:h-14 lg:h-16"
             />
           </div>
 
