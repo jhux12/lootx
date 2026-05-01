e8411cba05a279a9e61d63de1643907fef315cd4
jhux12 <79742669+jhux12@users.noreply.github.com>
2026-04-30 22:09:59 -0400

    Fix FirebaseUI import for Vite/Rollup compatibility

--- components/FirebaseAuthUI.tsx
+++ components/FirebaseAuthUI.tsx
@@ -1,22 +1,19 @@
 import { useEffect } from 'react';
 import { EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth';
-import firebaseui from 'firebaseui';
+import * as firebaseui from 'firebaseui';
 import 'firebaseui/dist/firebaseui.css';
 import { auth } from '../firebase';
 
 const FirebaseAuthUI = () => {
   useEffect(() => {
-    const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(auth);
+    const ui = firebaseui.auth.AuthUI.getInstance() ?? new firebaseui.auth.AuthUI(auth);
 
     console.info('FirebaseUI starting');
 
     ui.start('#firebaseui-auth-container', {
       signInFlow: 'redirect',
       signInSuccessUrl: '/',
-      signInOptions: [
-        GoogleAuthProvider.PROVIDER_ID,
-        EmailAuthProvider.PROVIDER_ID,
-      ],
+      signInOptions: [GoogleAuthProvider.PROVIDER_ID, EmailAuthProvider.PROVIDER_ID],
       callbacks: {
         signInSuccessWithAuthResult: () => {
           console.info('FirebaseUI sign-in success');
